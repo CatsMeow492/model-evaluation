dfdd5b58320e1095c75abbdfa4430dc82bfcb2ac
Catsmeow492 <taylormohney@icloud.com>
2021-11-11 13:42:39 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+tp, fp, fn, tn = [int(x) for x in input().split()]
+
+total = tp+ fp+ fn+ tn
+
+accuracy = (tp + tn )/(total)  #round(x, 4)
+precision = tp/ (tp+fp)
+recall = tp/ (tp+fn)
+f1 = round(1/(((1/precision) + (1/recall)) / 2), 4)
+
+print(round(accuracy, 4))
+print(round(precision, 4))
+print(round(recall, 4))
+print(f1)
